bc10cee9da68406505f457ff24e3c1f619378de1
apple <taiwooluwasegun02@gmail.com>
2022-09-14 01:40:08 +0100

    thumbnails and responsive grid setup

--- components/Nav.js
+++ components/Nav.js
@@ -7,8 +7,10 @@ function Nav() {
         <div className="flex px-10 py-5 sm:px-20 text-1xl whitespace-nowrap space-x-10 sm:space-x-20s overflow-x-scroll scrollbar-hide">{
             Object.entries(requests).map(([key, {title,url}])=>
             (
-                <h2 onClick={()=>router.push(`/?genres=${key}`)}
-                key={key} className=" last:pr-24 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500 ">
+                <h2 
+                key={key}
+                onClick={()=>router.push(`/?genre=${key}`)}
+                className=" last:pr-24 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500 ">
                     {title}
                 </h2>
             ))
--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,9 +1,10 @@
 import Image from "next/image"
 import { HandThumbUpIcon } from "@heroicons/react/24/outline"
-function Thumbnail({result}) {
+import { forwardRef } from "react"
+const Thumbnail = forwardRef(({result}, ref) => {
     const BASE_URL = "https://image.tmdb.org/t/p/original/"
   return (
-    <div className="group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+    <div ref={ref} className="group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
         <Image
         layout="responsive"
         src={
@@ -15,10 +16,10 @@ function Thumbnail({result}) {
         
         />
         <div className="p-2">
-            <p className="truncate max-w-md">{result.overview}</p>
-            <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
+            <h2 className="mt-1  text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
                 {result.title || result.original_name}
             </h2>
+            <p className="truncate max-w-md">{result.overview}</p>
             <p className="flex items-center opacity-0 group-hover:opacity-100">
                 {result.media_type && `${result.media_type} .` }{""}
                 {result.release_date || result.first_air_date} . {""}
@@ -29,6 +30,6 @@ function Thumbnail({result}) {
 
     </div>
   )
-}
+})
 
 export default Thumbnail
